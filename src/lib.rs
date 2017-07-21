dbf2292fee15aa5ff7268a56eb450d3c9cec8396
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-07-21 11:04:29 +0200

    Correct link to FITS wikipedia page

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(missing_docs)]
 //! An encoder and decoder for FITS images.
 //!
-//! The *Flexible Image Transport System* ([FITS][fits]) is
+//! The *Flexible Image Transport System* ([FITS](https://en.wikipedia.org/wiki/FITS)) is
 //! > an open standard defining a digital file format useful for storage,
 //! > transmission and processing of scientific and other images.
 
