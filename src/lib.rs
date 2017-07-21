0e835270f2514c7a19c347710f65086872ec733b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-07-21 08:10:55 +0200

    Deny missing docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![deny(missing_docs)]
+//! An encoder and decoder for FITS images.
+//!
+//! The *Flexible Image Transport System* ([FITS][fits]) is
+//! > an open standard defining a digital file format useful for storage,
+//! > transmission and processing of scientific and other images.
+
 #[cfg(test)]
 mod tests {
     #[test]
