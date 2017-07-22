095049deb47881cb5f1fac618002b92dc9847014
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-07-22 08:14:55 +0200

    Differentiate between primary header blocks and other blocks

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,7 @@
 //! Parser module is responsible for parsing FITS files.
 
-named!(fits<&[u8], Vec<&[u8]> >, many1!( take!(2880) ));
+named!(fits<&[u8], (Vec<&[u8]>, Vec<&[u8]>) >,
+       pair!(many_m_n!(2, 2, take!(2880) ), many0!( take!(2880) )));
 
 #[cfg(test)]
 mod tests {
@@ -14,7 +15,10 @@ mod tests {
         let result = fits(data);
 
         match result {
-            IResult::Done(_, blocks) => assert_eq!(blocks.len(), 3677),
+            IResult::Done(_, (header, blocks)) => {
+                assert_eq!(header.len(), 2);
+                assert_eq!(blocks.len(), 3675);
+            },
             IResult::Error(_) => panic!("Did not expect an error"),
             IResult::Incomplete(_) => panic!("Did not expect to be incomplete")
         }
