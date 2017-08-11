8396b2f0412a0e447b756ddaa3551c3ac6cf0db7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-08-11 09:29:25 +0200

    Read all header from a Fits file

---
+++ src/bin/headers.rs
@@ -0,0 +1,37 @@
+extern crate nom;
+extern crate fits_rs;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::str::FromStr;
+use nom::IResult;
+use fits_rs::parser::fits;
+use fits_rs::types::Header;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let header_index = u64::from_str(&args[2]).expect("second argument should be a non-negative number");
+
+    let mut f = File::open(filename).expect("file not found");
+    let mut buffer: Vec<u8> = vec!();
+    let _ = f.read_to_end(&mut buffer);
+
+    let result = fits(&buffer);
+
+    match result {
+        IResult::Done(_, trappist1) => {
+            let header: &Header = if header_index == 0 {
+                &trappist1.primary_hdu.header
+            } else {
+                &trappist1.extensions[0].header
+            };
+
+            for ref record in &header.keyword_records {
+                println!("{}", record);
+            }
+        },
+        _ => panic!("Whoops, something went wrong")
+    }
+}
